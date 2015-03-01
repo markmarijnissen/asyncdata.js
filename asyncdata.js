3bc9f40d46a5ea29a11f545b574a810261356b2b
Dimitris Theodorou <dimitris.theodorou@gmail.com>
2015-03-01 20:58:16 +0100

    add src, test, bower file

---
+++ asyncdata.js
@@ -0,0 +1,254 @@
+
+//some requirements:
+// an object with a loaded(success, failure, finally) method,  a
+// isLoading boolean flag, and a load() method
+// implementation:
+// initial state: isLoading is false
+
+
+(function(definition) {
+    if (typeof module !== 'undefined') {
+      module.exports = definition();
+    }
+    else if (typeof define === 'function' && define.amd) {
+      define(['signals'], definition);
+    }
+    else {
+      this['asyncData'] = definition(signals.Signal);
+    }
+}(function(Signal) {
+
+  /**
+   * Returns an object with a `resolved(success, failure, finally)` method,
+   * similar to a promise .then() . The loaded callbacks are triggered every
+   * time the `load()` method is resolved. `resolved` calls can be chained like
+   * promise .then() calls. each `resolved` in the chain is resolved with the
+   * value of the previous `resolved` success/failure callbacks.
+   *
+   * The object also has a `requested(callback)` method that will be
+   * triggered every time the `load()` method is invoked. An accompanying
+   * `isLoading` flag is also set when loading is started, and unset when
+   * loading is finished.
+   *
+   * The object also has a `progressed(callback)` method that is triggered while
+   * the data is being loaded.
+   *
+   * The sequence of events after a load() is called is:
+   * Events       |   State
+   * requested()  |   isLoading == true
+   * progressed() |   isLoading == true
+   * resolved()   |   isLoading == false
+   *
+   * If a load() is called while the object has not been resolved yet
+   *
+   * @param loadFn a function that returns a promise. The resolution of the
+   *    promise must return the loaded data.
+   * @return {AsyncDataSource}
+   *
+   * Example:
+   *
+   asyncData(function(){
+      return $http.get('api/data');
+    })
+   .resolved(function(data){
+      return transform(data);
+    })
+   .resolved(function(data){
+      console.log('got the transformed data', data)
+    });
+
+   data.load();
+   //this will trigger the promise resolution and then the transform.
+   //we must see:
+   //loading data...
+   //transforming data...
+   //got the transformed data
+
+   Requested can be used for notifications when the data is (re)loaded
+
+   var data = asyncData(function(){
+      return $http.get('api/data');
+    });
+
+   data.requested(function(progress){
+      console.log('loading data...')
+    });
+
+   var transformedData = data.resolved(function(data){
+      return transform(data);
+    });
+
+   transformedData.requested(function(){
+      console.log('transforming data...')
+    });
+
+   transformedData.resolved(function(data){
+      console.log('got the transformed data', data)
+    });
+
+   data.load();
+   //we will see:
+   //loading data...
+   //transforming data...
+   //got the transformed data
+
+   Requested can also be chained, and returns the same async data object it
+   operates on.
+
+   var data = asyncData(function(){
+      return $http.get('api/data');
+    })
+   .requested(function(progress){
+      console.log('loading data...')
+    })
+   .resolved(function(data){
+      return transform(data);
+    }).requested(function(){
+      console.log('transforming data...')
+    })
+   .resolved(function(data){
+      console.log('got the transformed data', data)
+    });
+
+   *
+   */
+  function asyncData(loadFn) {
+    return new AsyncDataSource(loadFn);
+  }
+
+  /**
+   * An object with `resolved()` and `requested()` signals that are re-triggered
+   * everytime the data it depends on is reloaded.
+   * @constructor
+   */
+  function AsyncData() {
+    this.isLoading = false;
+    this._loadingStarted = new Signal();
+    this._success = new Signal();
+    this._failure = new Signal();
+    this._finally = new Signal();
+    this._hasLoadedOnce = false;
+    this._lastSuccessData = undefined;
+    this._chainedAsyncData = [];
+  }
+
+  /**
+   * Register the success, failure, and finally callbacks
+   * for each respective outcome.
+   */
+  AsyncData.prototype.resolved = function (success, failure, finally_) {
+
+    var self = this;
+    var child = new AsyncData();
+    self._chainedAsyncData.push(child);
+
+    if (self._hasLoadedOnce) {
+      // if already fired, fire immediately on 'loaded' callback registration
+      var data = self._lastSuccessData;
+      if (success) {
+        data = success.call(self, data);
+      }
+      child._dispatchSuccess(data);
+    }
+    self._success.add(function (data) {
+      if (success) {
+        data = success.call(self, data);
+      }
+      child._dispatchSuccess(data);
+    });
+
+    self._failure.add(function (reason) {
+      if (failure) {
+        reason = failure.call(self, reason);
+      }
+      child._failure.dispatch(reason);
+    });
+
+    self._finally.add(function () {
+      if (finally_) {
+        finally_();
+      }
+      child.isLoading = false;
+      child._finally.dispatch();
+    });
+
+    return child;
+  };
+
+  /**
+   * Dispatch the signal that will trigger the `loadingStarted` callbacks for
+   * this AsyncData and all its chained children.
+   * @private
+   */
+  AsyncData.prototype._dispatchLoadingStarted = function () {
+    this.isLoading = true;
+    this._loadingStarted.dispatch();
+    for (i = 0; i < this._chainedAsyncData.length; i++) {
+      this._chainedAsyncData[i]._dispatchLoadingStarted();
+    }
+  };
+
+  /**
+   * Trigger the success callback, and memoize the success data.
+   * @private
+   */
+  AsyncData.prototype._dispatchSuccess = function (data) {
+    this._hasLoadedOnce = true;
+    this._lastSuccessData = data;
+    this._success.dispatch(data);
+  };
+
+
+  AsyncData.prototype.requested = function (callback) {
+    if (callback) {
+      this._loadingStarted.add(callback);
+    }
+    return this;
+  };
+
+  AsyncData.prototype.progressed = function (callback) {
+    //TODO: integrate with promise.notify()
+  };
+
+
+  /**
+   * Like AsyncData, but is the original data source and has a `load()` method.
+   *
+   * @param loadFn a function that must return a promise
+   * @constructor
+   */
+  function AsyncDataSource(loadFn) {
+    AsyncData.call(this);
+    this._load = loadFn;
+  }
+
+  AsyncDataSource.prototype = Object.create(AsyncData.prototype);
+  AsyncDataSource.prototype.constructor = AsyncDataSource;
+
+  AsyncDataSource.prototype.load = function () {
+    // what about concurrent loads?
+    var self = this;
+    this._dispatchLoadingStarted();
+
+    var promise = self._load();
+    if (promise.then == null || promise['finally'] == null) {
+      console.error(promise);
+      throw new Error('The callback on \'AsyncData\' must return a promise, ' +
+      'it returned \'' + promise + '\' instead.')
+    }
+    promise.then(function (data) {
+      self._dispatchSuccess(data);
+    }, function (reason) {
+      self._failure.dispatch(reason);
+    });
+    promise['finally'](function () {
+      self.isLoading = false;
+      self._finally.dispatch();
+    });
+    return promise;
+  };
+
+
+  return asyncData;
+
+}));
---
+++ asyncdata.spec.js
@@ -0,0 +1,502 @@
+describe("AsyncData", function() {
+  'use strict';
+  var deferred;
+  var data;
+
+  beforeEach(function() {
+    mockPromises.install(Q.makePromise);
+    mockPromises.reset();
+    deferred = Q.defer();
+    data = asyncData(function(){
+      return deferred.promise;
+    });
+  });
+
+  describe("should trigger ", function(){
+
+    var successCb;
+    var failureCb;
+    var finalyCb;
+    var loadPromise;
+
+    beforeEach(function(){
+      successCb = jasmine.createSpy('successCb');
+      failureCb = jasmine.createSpy('failureCb');
+      finalyCb = jasmine.createSpy('finalyCb');
+
+      data.resolved(successCb, failureCb, finalyCb);
+      loadPromise = data.load();
+
+      expect(successCb).not.toHaveBeenCalled();
+      expect(failureCb).not.toHaveBeenCalled();
+      expect(finalyCb).not.toHaveBeenCalled();
+    });
+
+    it("the loaded success callback", function() {
+
+      deferred.resolve([1,2]);
+
+      mockPromises.executeForPromise(deferred.promise);
+      mockPromises.executeForPromise(loadPromise);
+
+      expect(successCb).toHaveBeenCalledWith([1,2]);
+      expect(failureCb).not.toHaveBeenCalled();
+      expect(finalyCb).toHaveBeenCalledWith();
+
+    });
+
+    it("the loaded failure callback", function() {
+      deferred.reject('not happening');
+
+      mockPromises.executeForPromise(deferred.promise);
+      mockPromises.executeForPromise(loadPromise);
+
+      expect(successCb).not.toHaveBeenCalled();
+      expect(failureCb).toHaveBeenCalledWith('not happening');
+      expect(finalyCb).toHaveBeenCalledWith();
+
+    });
+  });
+
+  describe('with chained \'loaded\' should', function(){
+
+    var successCb;
+    var failureCb;
+    var finalyCb;
+    var loadPromise;
+
+    beforeEach(function(){
+      successCb = jasmine.createSpy('successCb');
+      failureCb = jasmine.createSpy('failureCb');
+      finalyCb = jasmine.createSpy('finalyCb');
+    });
+
+    describe('propagate the original data if no callbacks are added to the first \'loaded\'', function(){
+
+      beforeEach(function(){
+        data
+          .resolved()
+          .resolved(successCb, failureCb, finalyCb);
+        loadPromise = data.load();
+      });
+
+      it('on success', function(){
+
+        deferred.resolve(5);
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+        expect(successCb).toHaveBeenCalledWith(5);
+        expect(failureCb).not.toHaveBeenCalled();
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+
+      it('on failure', function(){
+
+        deferred.reject('no');
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+        expect(successCb).not.toHaveBeenCalled();
+        expect(failureCb).toHaveBeenCalledWith('no');
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+
+    });
+
+    describe('propagate the returned data of the callbacks of the first \'loaded\'', function(){
+      var successData;
+      var failureData;
+
+      beforeEach(function(){
+        data
+          .resolved(function(){
+            return successData;
+          }, function (){
+            return failureData;
+          }, function(){
+          })
+          .resolved(successCb, failureCb, finalyCb);
+        loadPromise = data.load();
+
+      });
+
+      it('on success', function(){
+
+        successData = 2;
+        deferred.resolve(5);
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+        expect(successCb).toHaveBeenCalledWith(2);
+        expect(failureCb).not.toHaveBeenCalled();
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+
+      it('on failure', function(){
+
+        failureData = 'fail';
+        deferred.reject('no');
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+
+        expect(successCb).not.toHaveBeenCalled();
+        expect(failureCb).toHaveBeenCalledWith('fail');
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+    });
+
+
+
+
+  });
+
+  describe('with doubly chained \'loaded\' should', function() {
+
+    var successCb;
+    var failureCb;
+    var finalyCb;
+    var loadPromise;
+
+    beforeEach(function () {
+      successCb = jasmine.createSpy('successCb');
+      failureCb = jasmine.createSpy('failureCb');
+      finalyCb = jasmine.createSpy('finalyCb');
+    });
+
+    describe('propagate the returned data of the callbacks of the second \'loaded\'', function () {
+
+      describe('when callbacks are added in the first \'loaded\'', function(){
+        var successData;
+        var failureData;
+        var secondSuccessData;
+        var secondFailureData;
+
+        beforeEach(function () {
+          data
+            .resolved(function () {
+              return successData;
+            }, function () {
+              return failureData;
+            }, function () {
+            })
+            .resolved(function(){
+              return secondSuccessData;
+            }, function(){
+              return secondFailureData;
+            })
+            .resolved(successCb, failureCb, finalyCb);
+          loadPromise = data.load();
+
+        });
+        it('on success', function(){
+
+          secondSuccessData = 2;
+          deferred.resolve(5);
+
+          mockPromises.executeForPromise(deferred.promise);
+          mockPromises.executeForPromise(loadPromise);
+
+          expect(successCb).toHaveBeenCalledWith(2);
+          expect(failureCb).not.toHaveBeenCalled();
+          expect(finalyCb).toHaveBeenCalledWith();
+
+        });
+
+        it('on failure', function(){
+
+          secondFailureData = 'fail';
+          deferred.reject('no');
+
+          mockPromises.executeForPromise(deferred.promise);
+          mockPromises.executeForPromise(loadPromise);
+
+
+          expect(successCb).not.toHaveBeenCalled();
+          expect(failureCb).toHaveBeenCalledWith('fail');
+          expect(finalyCb).toHaveBeenCalledWith();
+
+        });
+      });
+
+      describe('when no callbacks are added on first \'loaded\'', function(){
+
+        var secondSuccessData;
+        var secondFailureData;
+
+        beforeEach(function () {
+          data
+            .resolved()
+            .resolved(function(){
+              return secondSuccessData;
+            }, function(){
+              return secondFailureData;
+            })
+            .resolved(successCb, failureCb, finalyCb);
+          loadPromise = data.load();
+
+        });
+        it('on success', function(){
+
+          secondSuccessData = 2;
+          deferred.resolve(5);
+
+          mockPromises.executeForPromise(deferred.promise);
+          mockPromises.executeForPromise(loadPromise);
+
+          expect(successCb).toHaveBeenCalledWith(2);
+          expect(failureCb).not.toHaveBeenCalled();
+          expect(finalyCb).toHaveBeenCalledWith();
+
+        });
+
+        it('on failure', function(){
+
+          secondFailureData = 'fail';
+          deferred.reject('no');
+
+          mockPromises.executeForPromise(deferred.promise);
+          mockPromises.executeForPromise(loadPromise);
+
+
+          expect(successCb).not.toHaveBeenCalled();
+          expect(failureCb).toHaveBeenCalledWith('fail');
+          expect(finalyCb).toHaveBeenCalledWith();
+
+        });
+      });
+
+
+    });
+
+    describe('propagate the returned data of the callbacks of the first \'loaded\'', function () {
+
+      var successData;
+      var failureData;
+
+      beforeEach(function () {
+        data
+          .resolved(function () {
+            return successData;
+          }, function () {
+            return failureData;
+          }, function () {
+          })
+          .resolved()
+          .resolved(successCb, failureCb, finalyCb);
+        loadPromise = data.load();
+
+      });
+      it('on success', function(){
+
+        successData = 2;
+        deferred.resolve(5);
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+        expect(successCb).toHaveBeenCalledWith(2);
+        expect(failureCb).not.toHaveBeenCalled();
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+
+      it('on failure', function(){
+
+        failureData = 'fail';
+        deferred.reject('no');
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+
+        expect(successCb).not.toHaveBeenCalled();
+        expect(failureCb).toHaveBeenCalledWith('fail');
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+
+    });
+
+    describe('propagate the original data if no callbacks are added', function () {
+
+      beforeEach(function () {
+        data
+          .resolved()
+          .resolved()
+          .resolved(successCb, failureCb, finalyCb);
+        loadPromise = data.load();
+
+      });
+      it('on success', function(){
+
+        deferred.resolve(5);
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+        expect(successCb).toHaveBeenCalledWith(5);
+        expect(failureCb).not.toHaveBeenCalled();
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+
+      it('on failure', function(){
+
+        deferred.reject('no');
+
+        mockPromises.executeForPromise(deferred.promise);
+        mockPromises.executeForPromise(loadPromise);
+
+
+        expect(successCb).not.toHaveBeenCalled();
+        expect(failureCb).toHaveBeenCalledWith('no');
+        expect(finalyCb).toHaveBeenCalledWith();
+
+      });
+
+    });
+
+
+  });
+
+  it('\'load\' should throw Error when no promise is returned', function(){
+
+    var data = asyncData(2);
+
+    expect(data.load).toThrow();
+
+  });
+
+
+  describe('requested signals', function(){
+
+    var loadingCb1, loadingCb2, loadingCb3;
+
+
+    beforeEach(function(){
+      loadingCb1 = jasmine.createSpy('loadingCb1');
+      loadingCb2 = jasmine.createSpy('loadingCb2');
+      loadingCb3 = jasmine.createSpy('loadingCb3');
+    });
+
+    it('should be triggered for all chained results', function(){
+      data.requested(loadingCb1);
+      var data2 = data.resolved();
+      data2.requested(loadingCb2);
+      var data3 = data2.resolved();
+      data3.requested(loadingCb3);
+      data.load();
+
+      expect(data.isLoading).toBe(true);
+      expect(data2.isLoading).toBe(true);
+      expect(data3.isLoading).toBe(true);
+
+      expect(loadingCb1).toHaveBeenCalled();
+      expect(loadingCb2).toHaveBeenCalled();
+      expect(loadingCb3).toHaveBeenCalled();
+    });
+
+    it('should be possible to chain', function(){
+      data.requested(loadingCb1)
+        .resolved()
+        .requested(loadingCb2)
+        .resolved()
+        .requested(loadingCb3);
+      data.load();
+
+      expect(loadingCb1).toHaveBeenCalled();
+      expect(loadingCb2).toHaveBeenCalled();
+      expect(loadingCb3).toHaveBeenCalled();
+    })
+  });
+
+  describe('\'loaded\' should be immediately triggered for a chained result if the source has already been loaded', function(){
+
+    var successCb;
+    var failureCb;
+    var finalyCb;
+    var loadPromise;
+    var successData = [1,2];
+
+    beforeEach(function(){
+      successCb = jasmine.createSpy('successCb');
+      failureCb = jasmine.createSpy('failureCb');
+      finalyCb = jasmine.createSpy('finalyCb');
+
+      loadPromise = data.load();
+      deferred.resolve(successData);
+      mockPromises.executeForPromise(deferred.promise);
+      mockPromises.executeForPromise(loadPromise);
+
+    });
+
+    it('for one chained result', function(){
+
+      var result;
+      data.resolved(function(data){
+        result = data;
+      });
+      expect(result).toEqual(successData);
+    });
+
+    it('for two chained results', function(){
+
+      var result;
+      var data2 = data.resolved();
+      data2.resolved(function(data){
+        result = data;
+      });
+
+      expect(result).toEqual(successData);
+    });
+
+    it('for two chained results with intermediate result', function(){
+
+      var result;
+      var data2 = data.resolved(function(){
+        return 2;
+      });
+      data2.resolved(function(data){
+        result = data;
+      });
+
+      expect(result).toEqual(2);
+    });
+
+  });
+
+  it('second \'load\' should retrigger loaded', function(){
+
+    var successCb1 = jasmine.createSpy('successCb1');
+    var successCb2 = jasmine.createSpy('successCb2');
+
+    data.resolved(successCb1).resolved(successCb2);
+
+    var loadPromise = data.load();
+    deferred.resolve(2);
+    mockPromises.executeForPromise(deferred.promise);
+    mockPromises.executeForPromise(loadPromise);
+
+    expect(successCb1.calls.count()).toEqual(1);
+    expect(successCb2.calls.count()).toEqual(1);
+
+
+    loadPromise = data.load();
+    mockPromises.executeForPromise(deferred.promise);
+    mockPromises.executeForPromise(loadPromise);
+
+    expect(successCb1.calls.count()).toEqual(2);
+    expect(successCb2.calls.count()).toEqual(2);
+
+  });
+});
